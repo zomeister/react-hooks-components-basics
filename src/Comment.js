a385637caf671ef3b8527c83bca8d877aa363822
Zoe Guidry <oyezoey@gmail.com>
2022-11-03 21:33:17 -0500

    commit tree c0829428976fd45d5a40d933520a408bba1d6baf

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+/**
+ * Represents a comment
+ * @returns JSX element 
+ */
 function Comment() {
+  // build components with JSX, don't forget to
   return <div>Naturally, I agree with this article.</div>;
 }
 
+/**@export Comment: JSX Component */
 export default Comment;
